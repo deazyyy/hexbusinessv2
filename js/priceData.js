5843b989bee7484a51509f3020d11ffdea647ae2
getshwiftyinhere <60578750+getshwiftyinhere@users.noreply.github.com>
2020-06-15 06:43:00 +0100

    stats update
    display price data

--- js/priceData.js
+++ js/priceData.js
@@ -1109,17 +1109,36 @@ var ethHex = document.getElementById("ethHex");
 var ethHexChange = document.getElementById("ethHexChange");
 
 var hxyTotal = document.getElementById("hxyTotal");
-var hxyTotalUsd = document.getElementById("hxyTotalUsd");
+//var hxyTotalUsd = document.getElementById("hxyTotalUsd");
 var hxyTotalPercent = document.getElementById("hxyTotalPercent");
 var hxyFrozen = document.getElementById("hxyFrozen");
-var hxyFrozenUsd = document.getElementById("hxyFrozenUsd");
+//var hxyFrozenUsd = document.getElementById("hxyFrozenUsd");
 var hxyFrozenPercent = document.getElementById("hxyFrozenPercent");
 var hxyCirculating = document.getElementById("hxyCirculating");
-var hxyCirculatingUsd = document.getElementById("hxyCirculatingUsd");
+//var hxyCirculatingUsd = document.getElementById("hxyCirculatingUsd");
 var hxyCirculatingPercent = document.getElementById("hxyCirculatingPercent");
 var hxyLocked = document.getElementById("hxyLocked");
-var hxyLockedUsd = document.getElementById("hxyLockedUsd");
+//var hxyLockedUsd = document.getElementById("hxyLockedUsd");
 var hxyLockedPercent = document.getElementById("hxyLockedPercent");
+var hxyPrice = document.getElementById("hxyPrice");
+var hxyPriceChange = document.getElementById("hxyPriceChange");
+
+var hexTotal = document.getElementById("hexTotal");
+var hexTotalPercent = document.getElementById("hexTotalPercent");
+var hexStaked = document.getElementById("hexStaked");
+var hexStakedPercent = document.getElementById("hexStakedPercent");
+var hexCirculating = document.getElementById("hexCirculating");
+var hexCirculatingPercent = document.getElementById("hexCirculatingPercent");
+var hexPrice = document.getElementById("hexPrice");
+var hexPriceChange = document.getElementById("hexPriceChange");
+
+var aaHexRate = document.getElementById("aaHexRate");
+var aaHexChange = document.getElementById("aaHexChange");
+var aaHexRate2 = document.getElementById("aaHexRate2");
+var aaHexChange2 = document.getElementById("aaHexChange2");
+var aaPool = document.getElementById("aaPool");
+var aaPoolChange = document.getElementById("aaPoolChange");
+var countdown = document.getElementById("aaClosingTicker");
 
 GetData();
 
@@ -1173,40 +1192,134 @@ function GetData(){
            else{
             hexEthChange.className = "up";
            }
+           //HXY////////////////////////
            //overview
            var maxSupply = 60000000;
            //total
            var totalSupply = await hxyContract.methods.totalSupply().call();
            hxyTotal.innerHTML = toFixedMax((totalSupply / 10 ** 8), 1) + " HXY";
-           hxyTotalUsd.innerHTML =  "$" + "";
            var percentage = ((totalSupply / 10 ** 8) / maxSupply) * 100;
-           hxyTotalPercent.innerHTML = toFixedMax(percentage,2) + "% of max";
+           hxyTotalPercent.innerHTML = toFixedMax(percentage,2) + "% / max";
            //frozen
            var frozenSupply = await hxyContract.methods.getTotalFrozen().call();
            hxyFrozen.innerHTML = toFixedMax((frozenSupply / 10 ** 8), 1) + " HXY";
-           hxyFrozenUsd.innerHTML =  "$" + "";
            percentage = (frozenSupply / totalSupply) * 100;
-           hxyFrozenPercent.innerHTML = toFixedMax(percentage,3) + "% of total";
+           hxyFrozenPercent.innerHTML = toFixedMax(percentage,3) + "% / total";
            //locked
            var lockedSupply = await hxyContract.methods.getLockedSupply().call();
            hxyLocked.innerHTML = toFixedMax((lockedSupply / 10 ** 8), 1) + " HXY";
-           hxyLockedUsd.innerHTML =  "$" + "";
            percentage = (lockedSupply / totalSupply) * 100;
-           hxyLockedPercent.innerHTML = toFixedMax(percentage,2) + "% of total";
+           hxyLockedPercent.innerHTML = toFixedMax(percentage,2) + "% / total";
            //circulating
            var circulatingSupply = parseInt(totalSupply) - (parseInt(frozenSupply) + parseInt(lockedSupply));
            hxyCirculating.innerHTML = toFixedMax((circulatingSupply / 10 ** 8), 1) + " HXY";
-           hxyCirculatingUsd.innerHTML =  "$" + "";
            percentage = (circulatingSupply / totalSupply) * 100;
-           hxyCirculatingPercent.innerHTML = toFixedMax(percentage,2) + "% of total";
+           hxyCirculatingPercent.innerHTML = toFixedMax(percentage,2) + "% / total";
+           //price
+           var tRound = await hxyContract.methods.getCurrentHxyRound().call();
+           tRound += 2;
+           var _hxyPrice = data.hexUsd;
+           hxyPrice.innerHTML = "$" + toFixedMax(parseFloat(_hxyPrice) * (1000 * tRound), 2) + " per HXY";
+           hxyPriceChange.innerHTML = toFixedMax(data.hexUsd24Change, 2) + "%";
+           if(data.hexUsd24Change < 0){
+            hxyPriceChange.className = "down";
+           }
+           else{
+            hxyPriceChange.className = "up";
+           }
+           //HEX///////////////////
+           //total
+           var totalSupply = data.hexTotalSupply;
+           hexTotal.innerHTML = toFixedMax(totalSupply, 1) + " HEX";
+           hexTotalPercent.innerHTML =  "100%";
+           //staked
+           var stakedSupply = data.hexLockedSupply;
+           hexStaked.innerHTML = toFixedMax(stakedSupply, 1) + " HEX";
+           percentage = (stakedSupply / totalSupply) * 100;
+           hexStakedPercent.innerHTML = toFixedMax(percentage,3) + "% / total";
+           if(data.hexLockedSupply24Change < 0){
+            hexStakedPercent.className = "down";
+           }
+           else{
+            hexStakedPercent.className = "up";
+           }
+           //circulating
+           var circulatingSupply = data.hexCirculatingSupply;
+           hexCirculating.innerHTML = toFixedMax(circulatingSupply, 1) + " HEX";
+           percentage = (circulatingSupply / totalSupply) * 100;
+           hexCirculatingPercent.innerHTML = toFixedMax(percentage,2) + "% / total";
+           if(data.hexCirculatingSupply24Change < 0){
+            hexCirculatingPercent.className = "down";
+           }
+           else{
+            hexCirculatingPercent.className = "up";
+           }
+           //price
+           var _hexPrice = data.hexUsd;
+           hexPrice.innerHTML = "$" + toFixedMax(_hexPrice, 5);
+           hexPriceChange.innerHTML = toFixedMax(data.hexUsd24Change ,2) + "%";
+           if(data.hexUsd24Change < 0){
+            hexPriceChange.className = "down";
+           }
+           else{
+            hexPriceChange.className = "up";
+           }
+           //lobby close
+           var today = new Date(new Date().getTime());
+           var day = today.getDate() + 1;
+           var month = today.getMonth();
+           var year = today.getFullYear();
+           console.log(day + "/" + month + "/" + year);
+           var aaClose = Date.UTC(year, month, day);
+           console.log(aaClose);
+           countdown.innerHTML = CalcTimeTill(aaClose);
+           
+           //aa eth
+           var aaPoolValue = data.adoptionAmplifierCurrentEth;
+           aaPool.innerHTML = toFixedMax(aaPoolValue, 2) + " ETH";
+           aaPoolChange.innerHTML = toFixedMax(data.adoptionAmplifierCurrentEth24Change, 2) + "%";
+           if(data.adoptionAmplifierCurrentEth24Change < 0){
+            aaPoolChange.className = "down";
+           }
+           else{
+            aaPoolChange.className = "up";
+           }
+           //hex rate
+           var _aaHexRate = data.adoptionAmplifierCurrentHexEth;
+           aaHexRate.innerHTML = toFixedMax(_aaHexRate, 1) + " H/E";
+           aaHexChange.innerHTML = toFixedMax(data.adoptionAmplifierCurrentHexEth24Change,2) + "%";
+           aaHexRate2.innerHTML = toFixedMax(_aaHexRate, 1) + " HEX/ETH";
+           aaHexChange2.innerHTML = toFixedMax(data.adoptionAmplifierCurrentHexEth24Change,2) + "%";
+           if(data.adoptionAmplifierCurrentHexEth24Change < 0){
+            aaHexChange.className = "down";
+            aaHexChange2.className = "down";
+           }
+           else{
+            aaHexChange.className = "up";
+            aaHexChange2.className = "up";
+           }
         }
     };
     xhttp.open("GET", "https://hexvisionbusinessapi.azurewebsites.net/api/extendedStats", true);
     xhttp.send();
 }
 
-
+function CalcTimeTill(timestamp) {
+  var now = Date.now();
+  var seconds = (parseInt(timestamp) - parseInt(now)) / 1000;
+  var minutes = seconds / 60;
+  var hours = minutes / 60;
+  var days = hours / 24;
+  if (minutes < 1) {
+    return seconds.toFixed() + "s till close";
+  } else if (hours < 1) {
+    return minutes.toFixed() + "m till close";
+  } else if (days < 1) {
+    return toFixedMax(hours, 1) + "h till close";
+  }
+}
 
 function toFixedMax(value, dp) {
-    return +parseFloat(value).toFixed(dp);
-  }
+  return +parseFloat(value).toFixed(dp);
+}
+  
